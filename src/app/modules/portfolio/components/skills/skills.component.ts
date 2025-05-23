1ad396d9309ba6a12c552ffc510e51f31ae5e0c7
mandilorenzo <amanda.lorenzo@hotmail.com>
2025-05-23 10:11:31 -0300

    feat: criação e estilização do componente skillks

---
+++ src/app/modules/portfolio/components/skills/skills.component.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills',
+  imports: [CommonModule],
+  templateUrl: './skills.component.html',
+  styleUrl: './skills.component.css'
+})
+export class SkillsComponent {
+  skills = [
+    { name: 'HTML', icon: 'devicon-html5-plain colored' },
+    { name: 'CSS', icon: 'devicon-css3-plain colored' },
+    { name: 'JavaScript', icon: 'devicon-javascript-plain colored' },
+    { name: 'TypeScript', icon: 'devicon-typescript-plain colored' },
+    { name: 'Angular', icon: 'devicon-angularjs-plain colored' },
+    { name: 'React', icon: 'devicon-react-plain colored' },
+    { name: 'GitHub', icon: 'devicon-github-original' },
+    { name: 'Git', icon: 'devicon-git-plain colored' },
+    { name: 'Node.js', icon: 'devicon-nodejs-line' },
+    { name: 'Express.js', icon: 'devicon-express-original colored' },
+    { name: 'Prisma', icon: 'devicon-prisma-plain colored' },
+    { name: 'PostgreSQL', icon: 'devicon-postgresql-plain' }
+  ];
+
+  async ngAfterViewInit() {
+    if (typeof window !== 'undefined') {
+      const ScrollReveal = (await import('scrollreveal')).default;
+      ScrollReveal().reveal('.animation', {
+        distance: '50px',
+        duration: 1000,
+        origin: 'bottom',
+        reset: true,
+        viewFactor: 0.4,
+        mobile: true,
+        cleanup: true,
+      });
+    }
+  }
+}
--- src/app/modules/portfolio/pages/home/home.component.ts
+++ src/app/modules/portfolio/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { ProfileComponent } from '../../components/profile/profile.component';
 import { AboutComponent } from '../../components/about/about.component';
+import { SkillsComponent } from '../../components/skills/skills.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, ProfileComponent,AboutComponent],
+  imports: [HeaderComponent, ProfileComponent,AboutComponent, SkillsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
