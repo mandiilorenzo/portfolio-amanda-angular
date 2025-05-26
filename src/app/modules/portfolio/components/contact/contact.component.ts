519df25824cbe4632127fe0c6f24f6ab6dd21611
mandilorenzo <amanda.lorenzo@hotmail.com>
2025-05-25 22:21:03 -0300

    feat: criação e estilização do footer

--- src/app/modules/portfolio/components/contact/contact.component.ts
+++ src/app/modules/portfolio/components/contact/contact.component.ts
@@ -1,15 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import Player from 'lottie-web';
 import { AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app-contact',
+  standalone: true,
+  imports: [ CommonModule],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
   options: AnimationOptions = {
-    path: 'assets/animations/dev-email.json', 
+    path: 'assets/animations/manifest.json', 
     loop: true,
     autoplay: true,
   };
--- src/app/modules/portfolio/pages/home/home.component.ts
+++ src/app/modules/portfolio/pages/home/home.component.ts
@@ -5,10 +5,11 @@ import { AboutComponent } from '../../components/about/about.component';
 import { SkillsComponent } from '../../components/skills/skills.component';
 import { ProjectsComponent } from '../../components/projects/projects.component';
 import { ContactComponent } from '../../components/contact/contact.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, ProfileComponent,AboutComponent, SkillsComponent, ProjectsComponent, ContactComponent],
+  imports: [HeaderComponent, ProfileComponent,AboutComponent, SkillsComponent, ProjectsComponent, ContactComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
