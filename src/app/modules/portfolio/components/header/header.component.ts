3ee0989bbedd0ac86652c2de78a2771c4da93674
mandilorenzo <amanda.lorenzo@hotmail.com>
2025-06-05 11:32:14 -0300

    style: responsivo e light theme do header

--- src/app/modules/portfolio/components/header/header.component.ts
+++ src/app/modules/portfolio/components/header/header.component.ts
@@ -1,14 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+
   scrollToSection(event: Event, id: string) {
-  event.preventDefault();
-  document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
-}
+    event.preventDefault();
+    document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
+  }
 }
